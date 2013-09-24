40df30ad325aad9573a01d377535ee4122908226
John Rozger <jdr22@njit.edu>
2013-09-23 20:38:44 -0400

    got authentication working

---
+++ middle/moodleauth2.php
@@ -0,0 +1,48 @@
+<?php
+
+//extract data from the post
+extract($_POST);
+
+// add the extra input variables the POST request expects
+$__VIEWSTATE = '/wEPDwUJNDIzOTY1MjU5ZGQdLVY+81xpmN0ATE7y41EHAhVaCA==';
+$btnLogin = 'Login';
+$__EVENTVALIDATION = '/wEWBAK7zbGBDQLr9O+IBwK01ba+BAKC3IeGDOn1GTxupWw9xfJhOXrBSFX6INdC' ;
+
+//set POST variables
+$url = 'https://moodleauth00.njit.edu/cpip_serv/login.aspx?esname=moodle';
+$fields = array(
+						'__VIEWSTATE' => urlencode($__VIEWSTATE),
+						'txtUCID' => urlencode($username),
+						'txtPasswd' => urlencode($password),
+						'btnLogin' => urlencode($btnLogin),
+						'__EVENTVALIDATION' => urlencode($__EVENTVALIDATION)
+				);
+
+//url-ify the data for the POST
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//execute post
+$result = curl_exec($ch);
+
+if (strpos($result,'Object moved') !== false) {
+    echo 'authenticated';
+	}
+else
+	echo 'not authenticated' ;
+
+//close connection
+curl_close($ch);
+
+?>
+

c0aef43b0ebd9c17f2e855d986245c8034d2afb9
John Rozger <jdr22@njit.edu>
2013-11-25 19:24:22 -0500

    added main redirect script

---
+++ middle/toMainMenu.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$crn = $_SESSION['crnNumber'];
+?>
+<html>
+<form id="form" action="http://web.njit.edu/~ac422/cs490/front/professorWelcome2.php" method="POST">
+<input type="hidden" name="dropDown" value="<?php echo $crn ?>">
+</form>
+<script>
+document.getElementById("form").submit();
+</script>
+</html>
